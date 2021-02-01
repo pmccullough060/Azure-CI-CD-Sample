c5c6498c112de8d182e2b4f2df9b043eb8bbff6d
Peter McCullough <peter.mccullough@atkinsglobal.com>
2021-02-01 11:36:54 +0000

    first commit

---
+++ Controllers/MainController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PeterAzureConfig.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MainController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            //configuring the blob storage logging.
+
+            Trace.WriteLine("Http Get Request Triggered");
+            Trace.TraceInformation("Http Get Request Triggered");
+            Trace.TraceError("Http Get Request Triggered");
+            Trace.TraceWarning("Http Get Request Triggered");
+
+            return new OkObjectResult("Hello");
+        }
+
+        [HttpGet("{input}")] //passes this in as an argument 
+        public IActionResult Get(string input)
+        {
+            //configuring the blob storage logging.
+
+            Trace.WriteLine($"Http Get Request Triggered {input}");
+            Trace.TraceInformation($"Http Get Request Triggered {input}");
+            Trace.TraceError($"Http Get Request Triggered {input}");
+            Trace.TraceWarning($"Http Get Request Triggered {input}");
+
+            return new OkObjectResult(input);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PeterAzureConfig
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
